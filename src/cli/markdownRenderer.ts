b87dd780cc07869ebfb52c27bb24d751a7e4ec0d
Peter Steinberger <steipete@gmail.com>
2025-11-16 21:02:14 +0100

    Make renderer options non-enumerable for marked

--- src/cli/markdownRenderer.ts
+++ src/cli/markdownRenderer.ts
@@ -7,6 +7,17 @@ const renderer = new TerminalRenderer({
   tab: 2,
 });
 
+// Make terminal renderer's options non-enumerable to satisfy Marked's renderer shape checks.
+if (renderer && Object.prototype.hasOwnProperty.call(renderer as object, 'options')) {
+  const optionsValue = (renderer as unknown as { options?: unknown }).options;
+  Object.defineProperty(renderer, 'options', {
+    value: optionsValue,
+    enumerable: false,
+    writable: true,
+    configurable: true,
+  });
+}
+
 const markedWithTerminal = new Marked({ renderer: renderer as unknown as any });
 
 /**
